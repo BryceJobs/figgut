22e373f1dd01e62e98c41281ba532469d6545130
Prelang Builder <builder@prelang.com>
2014-04-11 06:06:30 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Figgut::Application.config.secret_key_base = '2f1ace9b98c159b5e1df1d0f83f91ead68cd767537c669d3c7ffd4fdf8ee4671ae633793fe70997db7c872f76ca83a9862b831c57eabb940cc1127ba85f6c94d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Figgut::Application.config.session_store :cookie_store, key: '_figgut_session'
